f91e27afac862da701d356aa9c07ebfeb1301ebb
jordles113 <jordan.t.romero@gmail.com>
2021-03-26 14:37:27 -0500

    creates a working kafka-consumer

--- src/main/java/com/github/jordles113/kafka/tutorial1/ConsumerDemo.java
+++ src/main/java/com/github/jordles113/kafka/tutorial1/ConsumerDemo.java
@@ -1,10 +1,16 @@
 package com.github.jordles113.kafka.tutorial1;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerDemo {
@@ -13,6 +19,7 @@ public class ConsumerDemo {
 
         String bootstrapServers = "localhost:9092";
         String groupId = "my-fourth-application";
+        String topic = "first_topic";
 
         Properties properties = new Properties();
 
@@ -24,9 +31,18 @@ public class ConsumerDemo {
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         //create consumer
-
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
         //subscribe consumer to our topic(s)
-
+        consumer.subscribe(Arrays.asList(topic));
         // poll for new data
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("key: " + record.key() + "Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
     }
 }
